480788c27632442d2df9b5a5d0afba1247ea7eb2
jztimm <jztimm@gmail.com>
2024-08-04 15:33:53 -0400

    Properly render the left Sidebar and MobileNav components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,27 @@
+import Image from "next/image";
+import Sidebar from "../../components/Sidebar";
+import MobileNav from "../../components/MobileNav";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Jzavier", lastName: "Timm" };
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="felx size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" alt="menu icon" width={30} height={30} />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
