e835d859c416fefa9a91c217a43888f7cc53a6f1
jztimm <jztimm@gmail.com>
2024-08-04 12:59:51 -0400

    implament Home Page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import HeaderBox from "../../components/HeaderBox";
+import TotalBalanceBox from "../../components/TotalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Jzavier" };
+
+  return (
+    <div className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={1}
+            totalBanks={1}
+            totalCurrentBalance={2250.49}
+          />
+        </header>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
