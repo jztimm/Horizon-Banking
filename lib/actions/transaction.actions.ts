b2db0a09997fdb07a5e160139cd771f9627471ee
jztimm <jztimm@gmail.com>
2024-08-07 19:20:49 -0400

    implement Transfer Payment Page

---
+++ lib/actions/transaction.actions.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { ID } from "node-appwrite";
+import { createAdminClient } from "../appwrite";
+import { parseStringify } from "../utils";
+
+const {
+  APPWRITE_DATABASE_ID: DATABASE_ID,
+  APPWRITE_USER_COLLECTION_ID: USER_COLLECTION_ID,
+  APPWRITE_TRANSACTION_COLLECTION_ID: TRANSACTION_COLLECTION_ID,
+} = process.env;
+
+export const createTransaction = async (
+  transaction: CreateTransactionProps
+) => {
+  try {
+    const { database } = await createAdminClient();
+
+    const newTransaction = await database.createDocument(
+      DATABASE_ID!,
+      TRANSACTION_COLLECTION_ID!,
+      ID.unique(),
+      {
+        channel: "online",
+        category: "transfer",
+        ...transaction,
+      }
+    );
+
+    return parseStringify(newTransaction);
+  } catch (error) {
+    console.log(error);
+  }
+};
