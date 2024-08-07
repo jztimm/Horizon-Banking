49a1dafab21501b8d3aaa5ed36ed1088f21a5b59
jztimm <jztimm@gmail.com>
2024-08-07 19:28:43 -0400

    refactor: Enable transfer transaction retrieval from appwrite
    This commit enables the retrieval of transfer transactions from appwrite by uncommenting the necessary code. The transfer transactions are obtained using the getTransactionsByBankId function from the transaction.actions module. This change allows for the inclusion of transfer transactions in the list of all transactions, which are then sorted by date.

--- lib/actions/transaction.actions.ts
+++ lib/actions/transaction.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { createAdminClient } from "../appwrite";
 import { parseStringify } from "../utils";
 
@@ -28,6 +28,38 @@ export const createTransaction = async (
     );
 
     return parseStringify(newTransaction);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getTransactionsByBankId = async ({
+  bankId,
+}: getTransactionsByBankIdProps) => {
+  try {
+    const { database } = await createAdminClient();
+
+    const senderTransactions = await database.listDocuments(
+      DATABASE_ID!,
+      TRANSACTION_COLLECTION_ID!,
+      [Query.equal("senderBankId", bankId)]
+    );
+
+    const receiverTransactions = await database.listDocuments(
+      DATABASE_ID!,
+      TRANSACTION_COLLECTION_ID!,
+      [Query.equal("receiverBankId", bankId)]
+    );
+
+    const transaction = {
+      total: senderTransactions.total + receiverTransactions.total,
+      documents: [
+        ...senderTransactions.documents,
+        ...receiverTransactions.documents,
+      ],
+    };
+
+    return parseStringify(transaction);
   } catch (error) {
     console.log(error);
   }
