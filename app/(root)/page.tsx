906c205347a77e7b45411b84ee574709619b86a2
jztimm <jztimm@gmail.com>
2024-08-06 21:31:28 -0400

    implement Recent Transactions, Connecting Multiple Banks, and Transaction History Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,18 +4,18 @@ import TotalBalanceBox from "../../components/TotalBalanceBox";
 import RightSidebar from "@/components/RightSidebar";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
+import RecentTransactions from "@/components/RecentTransactions";
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
-  const loggedIn = await getLoggedInUser();
+  const currentPage = Number(page as string) || 1;
 
+  const loggedIn = await getLoggedInUser();
   const accounts = await getAccounts({ userId: loggedIn.$id });
 
   if (!accounts) return;
 
   const accountsData = accounts?.data;
-
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
-
   const account = await getAccount({ appwriteItemId });
 
   return (
@@ -35,7 +35,13 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
             totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
-        RECENT TRANSACTIONS
+
+        <RecentTransactions
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
       </div>
 
       <RightSidebar
